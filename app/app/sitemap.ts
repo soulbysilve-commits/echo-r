dcaa3266f9e9b286d6af629ecfe69eef9ddf0a77
SoulBySilver <soulbysilver@veritasforge.net>
2026-06-05 08:47:23 +0900

    Add home CTA and build sections

---
+++ app/app/sitemap.ts
@@ -0,0 +1,22 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    return [
+        {
+            url: "https://echo-r.veritasforge.net",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://echo-r.veritasforge.net/echo-r",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://echo-r.veritasforge.net/about",
+            lastModified: new Date(),
+        },
+        {
+            url: "https://echo-r.veritasforge.net/contact",
+            lastModified: new Date(),
+        },
+    ];
+}
